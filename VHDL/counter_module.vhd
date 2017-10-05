60e31b04026ba00374b24d28b6bee0168fb628fa
Koutetsu <Anes.Benmerzoug@gmail.com>
2017-10-05 12:06:09 +0100

    Added initial structure for the VHDL module

---
+++ VHDL/counter_module.vhd
@@ -0,0 +1,32 @@
+---------------------------------------------------------------------------------- 
+-- Create Date:    12:01:47 10/05/2017 
+-- Design Name: 
+-- Module Name:    counter_module - Behavioral 
+-- Project Name: 	 Counter
+-- Description:  
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+entity counter_module is
+    Port ( Clock : in  STD_LOGIC;
+           Reset : in  STD_LOGIC;
+           Count_en : in  STD_LOGIC;
+           Up_Down_Ctrl : in  STD_LOGIC;
+           Count_out : out  STD_LOGIC_VECTOR (16 downto 0);
+           Overflow_intr : out  STD_LOGIC;
+           Underflow_intr : out  STD_LOGIC);
+end counter_module;
+
+architecture Behavioral of counter_module is
+
+begin
+
+
+end Behavioral;
+
---
+++ VHDL/counter_testbench.vhd
@@ -0,0 +1,87 @@
+--------------------------------------------------------------------------------
+-- Create Date:   12:02:39 10/05/2017
+-- Design Name:   
+-- Module Name:   /home/koutetsu/Counter/VHDL/counter_testbench.vhd
+-- Project Name:  Counter
+-- 
+-- VHDL Test Bench for module: counter_module
+-- 
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY counter_testbench IS
+END counter_testbench;
+ 
+ARCHITECTURE behavior OF counter_testbench IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT counter_module
+    PORT(
+         Clock : IN  std_logic;
+         Reset : IN  std_logic;
+         Count_en : IN  std_logic;
+         Up_Down_Ctrl : IN  std_logic;
+         Count_out : OUT  std_logic_vector(16 downto 0);
+         Overflow_intr : OUT  std_logic;
+         Underflow_intr : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Clock : std_logic := '0';
+   signal Reset : std_logic := '0';
+   signal Count_en : std_logic := '0';
+   signal Up_Down_Ctrl : std_logic := '0';
+
+ 	--Outputs
+   signal Count_out : std_logic_vector(16 downto 0);
+   signal Overflow_intr : std_logic;
+   signal Underflow_intr : std_logic;
+
+   -- Clock period definitions
+   constant Clock_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: counter_module PORT MAP (
+          Clock => Clock,
+          Reset => Reset,
+          Count_en => Count_en,
+          Up_Down_Ctrl => Up_Down_Ctrl,
+          Count_out => Count_out,
+          Overflow_intr => Overflow_intr,
+          Underflow_intr => Underflow_intr
+        );
+
+   -- Clock process definitions
+   Clock_process :process
+   begin
+		Clock <= '0';
+		wait for Clock_period/2;
+		Clock <= '1';
+		wait for Clock_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for Clock_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
